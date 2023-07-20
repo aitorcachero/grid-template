9668b1c1f3581aafa7ccf10c2c61e94be376e2f3
Aitor Cachero <aitor.cachero@gmail.com>
2023-07-20 16:18:17 +0200

    search function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const arrayFotos = document.querySelectorAll("img")
+const arrayLista = document.querySelectorAll("li")
 const arrayNombres = document.querySelectorAll("h2")
 const arrayCorreos = document.querySelectorAll("h4")
 const arrayPhones = document.querySelectorAll("p")
-
-
+const input = document.querySelector("input")
 
 function data() {
 
@@ -16,12 +16,30 @@ function data() {
         arrayCorreos[i].innerHTML = json.results[i].email
         arrayPhones[i].innerHTML = json.results[i].phone
       }
+      input.oninput = () => {
+        const arrayFullNombres = []
+        const inputLower = input.value.toLowerCase()
+        for (let i = 0; i < arrayNombres.length; i++) {
+          arrayFullNombres.push(arrayNombres[i].innerHTML.toLowerCase())
+        }
+        const arrayFilter = arrayFullNombres.filter(x => x.includes(inputLower))
+        for (let i = 0; i < arrayNombres.length; i++) {
+          if (arrayFilter.includes(arrayFullNombres[i]))
+            arrayLista[i].style.display = 'list-item';
+        else
+            arrayLista[i].style.display = 'none';
+
+
+        }
+        console.log(arrayFilter)
+      }
     })
 
 }
 
-
 data()
+
+
 
 
 
