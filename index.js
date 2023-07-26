3673151fd45160f0f3fa29e0d2296654bae2257d
Aitor Cachero <aitor.cachero@gmail.com>
2023-07-26 08:59:50 +0200

    Subo estilo de rotate

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const arrayNombres = document.querySelectorAll("h2")
 const arrayCorreos = document.querySelectorAll("h4")
 const arrayPhones = document.querySelectorAll("p")
 const input = document.querySelector("input")
+const deleteCards = document.querySelectorAll("h2, h4, p")
+
+
 
 function data() {
 
@@ -31,10 +34,37 @@ function data() {
             arrayLista[i].style.display = 'none';
         }
       }
+
+      arrayLista.forEach(element => {
+        element.addEventListener("mouseenter", () => {
+          console.log(element.childNodes)
+          element.childNodes[1].style.transform = "scale(2)"
+          element.childNodes[3].style.display = "none"
+          element.childNodes[5].style.display = "none"
+          element.childNodes[7].style.display = "none"
+          element.childNodes[9].style.display = "none"
+        })
+        element.addEventListener("mouseleave", () => {
+          console.log(element.childNodes)
+          element.childNodes[1].style.transform = "scale(1)"
+          element.childNodes[3].style.display = "list-item"
+          element.childNodes[5].style.display = "list-item"
+          element.childNodes[7].style.display = "list-item"
+          element.childNodes[9].style.display = "list-item"
+        })
+      })
     })
 }
 
 data()
+
+
+
+
+// fetch("https://randomuser.me/api/?page=3&results=100&seed=abc")
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+
 
 
 
